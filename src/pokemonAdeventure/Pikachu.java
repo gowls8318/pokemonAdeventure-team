1b7e0fab4432ecb7ef478180e03b1779eb8bf5bc
gowls8318 <68188378+gowls8318@users.noreply.github.com>
2022-01-08 22:27:15 +0900

    Update Pikachu.java

--- src/pokemonAdeventure/Pikachu.java
+++ src/pokemonAdeventure/Pikachu.java
@@ -8,7 +8,7 @@ public class Pikachu extends PokemonDTO {
 	public void recharge() {
 		// TODO Auto-generated method stub
 		if(this.hp < 100) {
-			System.out.println("[system] : 피카츄가 전기를 충전합니다. 피카츄의 HP가 100% 충전되었습니다.");
+			System.out.println("[system] : 피카츄(이)가 전기를 충전합니다. 피카츄의 HP가 100% 충전되었습니다.");
 			this.hp = 100;
 		} else {
 			System.out.println("[system] : 피카츄의 HP가 완충되어 있습니다. 공격을 진행해주세요.");
@@ -30,17 +30,17 @@ public class Pikachu extends PokemonDTO {
 			if (mission == count) {
 				Scanner sc = new Scanner(System.in);
 				while(true) {
-					System.out.print("진화 미션 횟수를 모두 채우셨습니다. 진화를 진행하시겠습니까? (y/n) : ");
+					System.out.print("[system] : 진화 미션 횟수를 모두 채우셨습니다. 진화를 진행하시겠습니까? (y/n) : ");
 					String yesOrNo = sc.nextLine();
 					
 					if(yesOrNo.equals("y")) {
-						System.out.println("피카츄가 라이츄로 진화했습니다. 축하드립니다.");
+						System.out.println("[system] : 피카츄(이)가 라이츄(으)로 진화했습니다. 축하드립니다!");
 						System.exit(0);
 					} else if (yesOrNo.equals("n")) {
-						System.out.println("진화를 못해 화가난 피카츄가 집을 나갔습니다ㅠㅠ. 게임을 종료합니다.");
+						System.out.println("[system] : 진화를 못해 화가난 피카츄(이)가 집을 나갔습니다ㅠㅠ. 게임을 종료합니다.");
 						System.exit(0);
 					} else {
-						System.out.println("y/n 중 하나만을 입력해야합니다. 다시 입력해주세요.");
+						System.out.println("[system] : y/n 중 하나만을 입력해야합니다. 다시 입력해주세요.");
 					}
 				
 				}
